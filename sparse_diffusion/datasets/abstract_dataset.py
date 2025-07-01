42aa93a0b5a9bd6011c5a25ac453b99746adaafc
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-07-01 08:46:55 -0700

    fix

--- sparse_diffusion/datasets/abstract_dataset.py
+++ sparse_diffusion/datasets/abstract_dataset.py
@@ -177,7 +177,7 @@ class AbstractDatasetInfos:
     def compute_input_dims(self, datamodule, extra_features, domain_features):
         data = next(iter(datamodule.train_dataloader()))
         print(data.x)
-        example_batch = self.to_one_hot(data)
+        example_batch = self.to_one_hot(data.long())
         ex_dense, node_mask = utils.to_dense(
             example_batch.x,
             example_batch.edge_index,
