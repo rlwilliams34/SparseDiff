0f76677cad77edba43f0c44d235df086b54ebae7
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-07-01 07:20:07 -0700

    test

--- sparse_diffusion/main.py
+++ sparse_diffusion/main.py
@@ -124,6 +124,9 @@ def main(cfg: DictConfig):
 
     ef = cfg.model.extra_features
     print("EF: ", ef)
+    ef = None
+    
+    
     edge_f = cfg.model.edge_features
     extra_features = (
         ExtraFeatures(
