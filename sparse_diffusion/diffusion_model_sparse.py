743c4362a213e1b1420981f3cd7aa1eb6eeb589c
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-07-08 06:43:17 -0700

    checking data

--- sparse_diffusion/diffusion_model_sparse.py
+++ sparse_diffusion/diffusion_model_sparse.py
@@ -180,6 +180,8 @@ class DiscreteDenoisingDiffusion(pl.LightningModule):
             return
         # Map discrete classes to one hot encoding
         data = self.dataset_info.to_one_hot(data)
+        print(data)
+        print(STOP)
 
         sparse_noisy_data = self.apply_sparse_noise(data)
         # Sample the query edges and build the computational graph = union(noisy graph, query edges)
