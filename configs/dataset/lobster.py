0b717eb9f18d3ca08e410b006056849c2d652d15
rlwilliams34 <rlwilliams34@g.ucla.edu>
2025-07-22 08:11:49 -0700

    750

--- configs/dataset/lobster.py
+++ configs/dataset/lobster.py
@@ -70,7 +70,7 @@ def tree_generator(num_leaves, seed = 285):
         n: number of leaves
         num_graphs: number of requested graphs
     '''
-    num_leaves = 1000
+    num_leaves = 750
     npr = np.random.RandomState(seed)
     py_random = random.Random(seed)
     g = tree_top_generator(num_leaves, py_random)
@@ -84,7 +84,7 @@ def tree_generator(num_leaves, seed = 285):
 
 
 class LobsterDataset(InMemoryDataset):
-    def __init__(self, root, split='train', n_bins=100, num_leaves=1000, seed=285, transform=None, pre_transform=None):
+    def __init__(self, root, split='train', n_bins=100, num_leaves=750, seed=285, transform=None, pre_transform=None):
         self.split = split
         self.n_bins = n_bins
         self.num_leaves = num_leaves
